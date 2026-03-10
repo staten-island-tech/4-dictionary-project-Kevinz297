fe062aaa596f2b57729865e527853ad7caa6cc3a
Kevinz297 <Kevinz297@nycstudents.net>
2026-03-10 09:22:16 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -41,6 +41,17 @@ def find_honi(sentence):
             state = 0
     print(count)
 
-find_honi("HHHHOOOONNNNIIII") """
+find_honi("HHHHOOOONNNNIIII")
 
-for
+
+"""  
+best_buy_items = [
+    {"name": "DDR5 32 gigabyte Ram", "price": 999.99, "description": "High-performance RAM for gaming and content creation."},
+    {"name": "Vbucks", "price": 499.99, "description": "In-game currency for Fortnite."},
+    {"name": "Social Credit", "price": 199.99, "description": "A measure of your online reputation."},
+    {"name": "Bitcoin", "price": 1.00, "description": "A decentralized digital currency."},
+    {"name": "Robux", "price": 399.99, "description": "In-game currency for Roblox."}
+]
+
+for index, item in enumerate(best_buy_items):
+    print(index, ":", item["name"])   
