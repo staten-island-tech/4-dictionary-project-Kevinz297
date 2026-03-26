5ed39ac10b48e1348a0f843458e473e57ca27961
Kevinz297 <Kevinz297@nycstudents.net>
2026-03-26 09:25:49 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -43,7 +43,7 @@ def find_honi(sentence):
 
 find_honi("HHHHOOOONNNNIIII")
 """
-Game_Currency = [
+""" Game_Currency = [
     {"name": "DDR5 32 gigabyte Ram", "price": 999.99, "description": "High-performance RAM for gaming and content creation."},
     {"name": "Vbucks", "price": 499.99, "description": "In-game currency for Fortnite."},
     {"name": "Social Credit", "price": 199.99, "description": "A measure of your online reputation."},
@@ -67,7 +67,7 @@ print("Items bought:")
 for item in Cart:
     print(item)
 print("Total:", total)
-
+ """
 """ def slot_m(q, m1, m2, m3):
     m1_played = m1
     m2_played = m2
@@ -96,3 +96,26 @@ print("Total:", total)
     x = m1_played-m1+m2_played-m2+m3_played-m3
     print(f"Martha played {x} times before going broke.")
 slot_m(77,4,9,3)  """ 
+
+def plantations (N, H):
+    current_increase_streak=1
+    current_decrease_streak=2
+    longest_increase_streak=1
+    longest_decrease_streak=1
+    for track in range(int(N)-1):
+        if H[track] < H[track+1]:
+            current_increase_streak += 1
+            if current_decrease_streak > longest_decrease_streak:
+                longest_decrease_streak = current_decrease_streak
+                current_decrease_streak = 1
+
+        elif H[track] > H[track+1]:
+            current_decrease_streak += 1
+            if current_increase_streak > longest_increase_streak:
+                longest_increase_streak = current_increase_streak
+                current_increase_streak = 1
+
+    print(longest_increase_streak)
+    print(longest_decrease_streak)
+
+plantations(4, [1, 3, 4, 2])
